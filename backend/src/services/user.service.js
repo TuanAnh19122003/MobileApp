89ea348e69cb6a17e507bec8bf830b712ae433d1
TuanAnh19122003 <gamethunghiem19122003@gmail.com>
2025-06-08 10:04:35 +0700

    thêm hàm mã hóa mật khẩu

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,3 +1,4 @@
+const hashPassword = require('../config/hashPassword');
 const Role = require('../models/role.model');
 const User = require('../models/user.model');
 
@@ -14,6 +15,9 @@ class UserService {
     }
 
     static async create(data) {
+        if(data.password){
+            data.password = await hashPassword(data.password);
+        }
         const user = await User.create(data);
         return user;
     }
@@ -34,6 +38,12 @@ class UserService {
     static async update(id, data) {
         const user = await User.findOne({ where: { id: id } });
         if (!user) throw new Error('User không tồn tại');
+
+        if(data.password && data.password !== user.password){
+            data.password = await hashPassword(data.password);
+        } else {
+            delete data.password;
+        }
         return await user.update(data)
     }
 
